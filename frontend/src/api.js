6e09904bed08169aebfd4dafbd3ddce47e098f51
Aarush <106799784+Aarush47@users.noreply.github.com>
2026-09-15 02:14:30 +0530

    Update frontend API url to new Catalyst account

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,5 +2,5 @@ const backendProtocol = window.location.protocol === 'https:' ? 'https' : 'http'
 const websocketProtocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
 const backendHost = window.location.hostname || 'localhost';
 
-export const API_BASE_URL = 'http://localhost:8000';
-export const WS_BASE_URL = 'ws://localhost:8000';
+export const API_BASE_URL = 'https://marg-50043781979.development.catalystappsail.in';
+export const WS_BASE_URL = 'wss://marg-50043781979.development.catalystappsail.in';
